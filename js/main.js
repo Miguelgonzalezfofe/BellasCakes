84fe6b8cb2d41d102abf423ad8c0d7c97fe947b8
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2024-07-17 07:09:13 -0300

    se creo base de datos, sectiones de las paginas(contacto, nosotros y preguntas frecuentes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
-// Despliegue del menu
+
+// Despliegue del menu  
 const toogleHeader= ()=>{
     let header = document.querySelector(".header")
-    header.classList.toggle("toogle_header")
+    header.classList.toggle("toggle_header")
 }
-
-// Eventos especiales
+// Despliegue Preguntas Frecuentes
+const togglePreguntas =()=>{
+    let preguntas = document.querySelectorAll("#preguntas")
+    console.log(preguntas)
+    preguntas.classList.toggle("toggle_pregunta")
+    console.log("si funciona el btn")
+}
+// Manejo del Dom -- Eventos
 let baseDatosEventos = [
     {
         nombre: "Boda",
-        img: "media/img/Combo_mesa_dulce_mixto(pequeño).jpg",    
+        img: "../media/img/Combo_mesa_dulce_mixto(pequeño).jpg",    
     },
     {
         nombre: "Bautizos",
-        img: "media/img/IMG-20240603-WA0019-min.jpg"
+        img: "../media/img/IMG-20240603-WA0019-min.jpg"
     },
     {
         nombre: "Cumpleaños",
-        img: "media/img/IMG-20240603-WA0004-min.jpg"
+        img: "../media/img/IMG-20240603-WA0004-min.jpg"
     }    
 ]
 
@@ -33,7 +40,7 @@ window.addEventListener("DOMContentLoaded",function(){
 
 // funcion para cargar evento
 const cargarEvento = ()=>{
-    const evento = baseDatosEventos[turno];
+    let evento = baseDatosEventos[turno]
     img.src = evento.img;
     nombre.textContent = evento.nombre
 }
@@ -53,3 +60,42 @@ let anterior = ()=>{
     }
     cargarEvento()
 }
+
+let content_productos = document.querySelector("#content_productos")
+console.log(content_productos)
+const cargarCatalogo = ()=>{
+    fetch("/Base_de_datos.json")
+    .then(Response => Response.json())
+    .then(Data =>{
+        let catalogo = Data.producto
+        catalogo.forEach(e=>{
+            let producto = document.createElement("a")
+            producto.href="../index.html"
+            producto.innerHTML = `
+                    <div class="producto">
+                        <div class="img_producto">
+                            <img src="../${e.img}" alt="">
+                        </div>
+                        <div class="info_producto">
+                            <p class="nombre">${e.nombre}</p>
+                            <p class="precio">${e.precio}</p>
+                        </div>
+                    </div>
+            `
+            content_productos.appendChild(producto)
+        })
+    })
+}
+
+// cargarCatalogo()
+
+
+function url (rul){
+    fetch(rul)
+    .then(Response => Response.json())
+    .then(Data=>[
+        console.log(Data)
+    ])
+}
+
+console.log(rul("../Base_de_datos.json"))
